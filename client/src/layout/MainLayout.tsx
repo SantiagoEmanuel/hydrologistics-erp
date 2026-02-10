d6b1f2b6185a90135a9adc9a620feb9c52e11988
santiago emanuel mustafa font <santiagomustafa3@gmail.com>
2026-02-10 09:13:51 -0300

    feat: implement user authentication check in MainLayout component

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,13 +1,20 @@
 import ShiftGuard from "@/components/layout/ShiftGuard";
 import Sidebar from "@/components/layout/Sidebar";
-import { Outlet } from "react-router";
+import { useAuth } from "@/hook/useAuth";
+import { Navigate, Outlet } from "react-router";
 import { Toaster } from "sonner";
 
 export default function MainLayout() {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <ShiftGuard>
       <div className="flex h-screen overflow-hidden bg-gray-50">
-        <aside className="h-full w-64 shrink-0 overflow-y-auto border-r border-gray-200 bg-white">
+        <aside className="h-full w-64 shrink-0 overflow-y-auto border-r border-gray-200 bg-white max-sm:hidden">
           <Sidebar />
         </aside>
         <main className="relative h-full flex-1 overflow-y-auto p-8">
