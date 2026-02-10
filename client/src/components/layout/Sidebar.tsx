e43a0d60689a62f8fb2727f5f1a4fa86b23a115d
santiago emanuel mustafa font <santiagomustafa3@gmail.com>
2026-02-10 09:11:46 -0300

    feat: restructure Sidebar component for improved layout and remove unused navigation links

--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -1,12 +1,15 @@
-import { navigationLinks } from "@/constants/navigation";
 import NavMenu from "./navigation/NavMenu";
 
 export default function Sidebar() {
   return (
     <header className="flex flex-col gap-4 rounded-md border-r border-r-gray-200 bg-white p-4">
-      <h1 className="text-2xl font-bold text-gray-500">HydroLogistics</h1>
+      <div>
+        <h1 className="text-2xl font-bold text-gray-500">HydroLogistics</h1>
+        <p className="text-xs font-semibold text-gray-500">Servicio ERP</p>
+      </div>
+
       <hr className="my-5 border-gray-300" />
-      <NavMenu items={navigationLinks} />
+      <NavMenu />
     </header>
   );
 }
