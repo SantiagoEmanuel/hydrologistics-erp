e8002108edd709daff67b97575fbc9f522813945
santiago emanuel mustafa font <santiagomustafa3@gmail.com>
2026-02-10 09:11:04 -0300

    feat: refactor routing structure and enhance authentication flow in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,67 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { useEffect } from "react";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
+import AuthGuard from "./components/AuthGuard";
+import { useAuth } from "./hook/useAuth";
+import AuthLayout from "./layout/AuthLayout";
 import MainLayout from "./layout/MainLayout";
 import AccountsReceivable from "./pages/accounts/AccountsReceivable";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Inventory from "./pages/Inventory/Inventory";
-import POS from "./pages/POS/POS";
-import RoutesPage from "./pages/routes/Router";
-import Settlements from "./pages/routes/Settlements";
-import SalesHistory from "./pages/sales/SalesHistory";
+import Login from "./pages/auth/Login";
+import AdminDashboard from "./pages/dashboard/admin/AdminDashboard";
+import UserManagement from "./pages/dashboard/admin/UserManagement";
+import Dashboard from "./pages/dashboard/Index";
+import Inventory from "./pages/dashboard/Inventory/Inventory";
+import POS from "./pages/dashboard/POS/POS";
+import RoutesPage from "./pages/dashboard/routes/Router";
+import Settlements from "./pages/dashboard/routes/Settlements";
+import SalesHistory from "./pages/dashboard/sales/SalesHistory";
 
 export default function App() {
+  const { checkAuth } = useAuth();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   const router = createBrowserRouter([
     {
+      path: "/auth",
+      Component: AuthLayout,
+      children: [{ path: "login", Component: Login }],
+    },
+    {
       path: "/",
-      Component: MainLayout,
+      Component: AuthGuard,
       children: [
         {
           index: true,
-          Component: Dashboard,
+          element: <Navigate to="/dashboard" replace />,
         },
         {
-          path: "pos",
-          Component: POS,
-        },
-        {
-          path: "inventory",
-          Component: Inventory,
-        },
-        {
-          path: "routes",
-          Component: RoutesPage,
-        },
-        {
-          path: "settlement",
-          Component: Settlements,
-        },
-        {
-          path: "accounts-receivable",
-          Component: AccountsReceivable,
-        },
-        {
-          path: "sales-history",
-          Component: SalesHistory,
+          path: "dashboard",
+          Component: MainLayout,
+          children: [
+            { index: true, Component: Dashboard },
+            { path: "pos", Component: POS },
+            { path: "sales-history", Component: SalesHistory },
+            { path: "routes", Component: RoutesPage },
+            { path: "settlement", Component: Settlements },
+            { path: "accounts-receivable", Component: AccountsReceivable },
+            { path: "admin/dashboard", Component: AdminDashboard },
+            {
+              path: "admin/dashboard/users",
+              Component: UserManagement,
+            },
+            {
+              path: "admin/dashboard/inventory",
+              Component: Inventory,
+            },
+          ],
         },
       ],
     },
+
+    { path: "*", element: <Navigate to="/auth/login" replace /> },
   ]);
 
-  return <RouterProvider router={router}></RouterProvider>;
+  return <RouterProvider router={router} />;
 }
